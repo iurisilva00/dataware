3b7a0f2037495b2490680552e6e61b5e0b435d90
Iuri da Silva <92988304+iurisilva00@users.noreply.github.com>
2024-09-14 10:23:37 -0300

    Subindo atualizações

--- vendas_dbt/models/models/gold/gold_vendas_7_dias.sql
+++ vendas_dbt/models/models/gold/gold_vendas_7_dias.sql
@@ -1,27 +1,7 @@
 {{ config(materialized='view') }}
 
-WITH vendas_7_dias AS (
-    SELECT 
-        data, 
-        produto, 
-        SUM(valor) AS total_valor, 
-        SUM(quantidade) AS total_quantidade, 
-        COUNT(*) AS total_vendas
-    FROM 
-        {{ ref('silver_vendas') }}
-    WHERE 
-        data >= CURRENT_DATE - INTERVAL '80 days'
-    GROUP BY 
-        data, produto
-)
 
-SELECT 
-    data, 
-    produto, 
-    total_valor, 
-    total_quantidade, 
-    total_vendas
-FROM 
-    vendas_7_dias
-ORDER BY 
-    data ASC
+    SELECT 
+    *
+    FROM 
+        {{ ref('bronze_vendas') }}
--- vendas_dbt/models/models/silver/silver_vendas.sql
+++ vendas_dbt/models/models/silver/silver_vendas.sql
@@ -1,18 +1,7 @@
 {{ config(materialized='view') }}
 
-WITH cleaned_data AS (
+
     SELECT 
-        email, 
-        DATE(data) AS data,
-        valor, 
-        quantidade, 
-        produto
+    *
     FROM 
         {{ ref('bronze_vendas') }}
-    WHERE 
-        valor > 0 
-     
-       
-)
-
-SELECT * FROM cleaned_data
